9cf18b526bd7a815d06e5c45c9391941c861d323
teoks0199 <teokaisheng3@gmail.com>
2023-05-17 23:38:05 +0800

    simple spell and target dummy

---
+++ DungeonBossSimulator/Assets/Scripts/TestProjectile.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestProjectile : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float damage;
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.name != "Player")
+        {
+            if (collision.GetComponent<EnemyReceiveDamage>() != null) {
+                collision.GetComponent<EnemyReceiveDamage>().DealDamage(damage);
+            }
+            //Destroy(gameObject);
+        }
+    }
+}
