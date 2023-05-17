dc3380d1503f044098f9d930b0021d6a1b6fda3f
teoks0199 <teokaisheng3@gmail.com>
2023-05-17 22:13:49 +0800

    update main menu

--- DungeonBossSimulator/Assets/MainMenu.cs
+++ DungeonBossSimulator/Assets/MainMenu.cs
@@ -3,10 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MainMenu : MonoBehaviour
-{
+public class MainMenu : MonoBehaviour {
+
+    public GameObject settingsPanel;
+
     public void StartGame() {
         SceneManager.LoadScene(1);
+    }
+
+    public void OpenSettings() {
+        settingsPanel.SetActive(true);
+    }
+
+    public void CloseSettings() {
+        settingsPanel.SetActive(false);
     }
 
     public void ExitGame() {
