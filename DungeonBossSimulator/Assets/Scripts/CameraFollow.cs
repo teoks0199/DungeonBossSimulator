2b5ea154e4149b5d56462fd85bae6eb5371a70c7
Keith Teo <keithteo@u.nus.edu>
2023-05-19 23:10:15 +0800

    Added Smooth Camera & Enemy Shooting

---
+++ DungeonBossSimulator/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform player;
+    public float smoothing;
+    public Vector3 offset;
+
+    void FixedUpdate() {
+        if(player != null) {
+            Vector3 newPosition = Vector3.Lerp(transform.position, player.transform.position + offset, smoothing);
+            transform.position = newPosition;
+        }
+    }
+
+    
+}
+
