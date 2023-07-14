5919a0981e3dcbc4c3d4c97882c26dea191c178c
Keith Teo <keithteo@u.nus.edu>
2023-07-14 20:50:46 +0800

    Added Boss

--- DungeonBossSimulator/Assets/Scripts/Upgrades/SummonMeleeMinionUpgrade.cs
+++ DungeonBossSimulator/Assets/Scripts/Upgrades/SummonMeleeMinionUpgrade.cs
@@ -6,11 +6,14 @@ using UnityEngine;
 public class SummonMeleeMinionUpgrade : Upgrade
 {
     public GameObject minion;
+    public GameObject minion2;
 
     public void upgrade()
     {
         minion = GameObject.Instantiate(PlayerStats.playerStats.meleeMinion, new Vector3(0, 0, 0), Quaternion.identity);
+        minion2 = GameObject.Instantiate(PlayerStats.playerStats.meleeMinion, new Vector3(1, 1, 1), Quaternion.identity);
         GameObject.DontDestroyOnLoad(minion);
+        GameObject.DontDestroyOnLoad(minion2);
     }
 
     
